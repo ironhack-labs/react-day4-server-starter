bfff50dd7fe9e5c7aae475ab812c71486f3f2368
sandrabosk <sandraboskovic@hotmail.com>
2018-09-17 23:52:37 -0400

    add user model, add passport, add app.js preset

---
+++ app.js
@@ -0,0 +1,80 @@
+require('dotenv').config();
+
+const bodyParser   = require('body-parser');
+const cookieParser = require('cookie-parser');
+const express      = require('express');
+const favicon      = require('serve-favicon');
+const hbs          = require('hbs');
+const mongoose     = require('mongoose');
+const logger       = require('morgan');
+const path         = require('path');
+
+
+// WHEN INTRODUCING USERS DO THIS:
+// INSTALL THESE DEPENDENCIES: passport-local, passport, bcryptjs, express-session, cors
+// AND UN-COMMENT OUT FOLLOWING LINES:
+
+// const cors          = require('cors');
+// const LocalStrategy = require('passport-local').Strategy;
+// const session       = require('express-session');
+// const passport      = require('passport');
+
+// require('./configs/passport');
+
+// IF YOU STILL DIDN'T, GO TO 'configs/passport.js' AND UN-COMMENT OUT THE WHOLE FILE
+
+mongoose
+  .connect('mongodb://localhost/project-management-server', {useNewUrlParser: true})
+  .then(x => {
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+  })
+  .catch(err => {
+    console.error('Error connecting to mongo', err)
+  });
+
+const app_name = require('./package.json').name;
+const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
+
+const app = express();
+
+// Middleware Setup
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// Express View engine setup
+
+app.use(require('node-sass-middleware')({
+  src:  path.join(__dirname, 'public'),
+  dest: path.join(__dirname, 'public'),
+  sourceMap: true
+}));
+      
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
+
+// ADD SESSION SETTINGS HERE:
+
+
+// USE passport.initialize() and passport.session() HERE:
+
+
+// default value for title local
+app.locals.title = 'Express - Generated with IronGenerator';
+
+
+// ADD CORS SETTINGS HERE TO ALLOW CROSS-ORIGIN INTERACTION:
+
+
+
+// ROUTES MIDDLEWARE STARTS HERE:
+
+const index = require('./routes/index');
+app.use('/', index);
+
+
+module.exports = app;
